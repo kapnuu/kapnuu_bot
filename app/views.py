425f4fb8ef99becd33c376047a68e488a6766c24
kapnuu <pryanichnikov.av@gmail.com>
2017-04-22 12:55:45 +0300

    add /now

--- app/views.py
+++ app/views.py
@@ -47,29 +47,38 @@ def process_request():
             message = data['message']
             chat_id = message['chat']['id']
 
-            text = message['text']
+            text = message['text'].lower()
             if text.startswith('/start'):
-                return start_f(chat_id)
+                return start_f(chat_id, message["from"].get("first_name"))
             elif text == '/help':
                 pass
             elif text == '/weather':
                 return weather_f(chat_id)
             elif text == '/currency':
                 return currency_f('usd', chat_id)
-            elif text.lower() == 'замучить котов' or text.lower() == 'torture cats':
+            elif text == 'замучить котов'\
+                    or text == 'мучить котов'\
+                    or text == 'torture cats':
                 return send_reply({'chat_id': chat_id, 'text': 'КОТОВ МУЧИТЬ НЕЛЬЗЯ, СУЧКА!!'})
+            elif text == '/now' or text == 'what time is it?':
+                return now_f(chat_id)
             else:
-                return send_reply({'chat_id': chat_id, 'text': 'You said: %s. WTF?' % text})
+                return send_reply({'chat_id': chat_id, 'text': 'You said: %s. WTF?' % message['text']})
 
     abort(400)
 
 
 @app.route('/start')
-def start_f(chat_id=None):
+def start_f(chat_id=None, who=None):
 
-    responses = ['Hello!', 'Hi there.', 'Дратути!', 'Превед!!1111адинвдин', 'Привет!', 'Hi!', 'Ксюшенька-пампушенька, любищь тебя, дурочку!']
+    responses = ['Hello, %s!',
+                 'Hi there, %s.',
+                 'Дратути, %s!',
+                 'Превед %s!!1111адинвдин',
+                 'Привет, %s!',
+                 'Hi, %s!']  # , 'Ксюшенька-пампушенька, любищь тебя, дурочку!']
 
-    hi = random.choice(responses)
+    hi = random.choice(responses) % (who if who else 'Human')
 
     resp = '''%s
 Sorry, I am not very useful bot, I'm just a my creator's helper: now he is learning how to develop telegram bots.
@@ -79,6 +88,7 @@ You can get info by sending these commands:
 /start — this message
 /weather — get current weather in Nizhniy Novgorod (other cities TBD)
 /currency — get USD to RUB rate (other currencies TBD)
+/now — get current date and time in utc
 
 Thanks, <i>kapnuu bot</i>
 
@@ -86,7 +96,7 @@ Thanks, <i>kapnuu bot</i>
 
     if chat_id:
         return send_reply({'chat_id': chat_id, 'parse_mode': 'html', 'text': resp})
-    return '<h1>%s</h1>' % resp
+    return '<pre>%s</pre>' % resp
 
 
 @app.route('/weather', methods=['GET'])
@@ -119,4 +129,15 @@ def currency_f(iso, chat_id=None):
 
     if chat_id:
         return send_reply({'chat_id': chat_id, 'parse_mode': 'html', 'text': resp})
+    return '<h1>%s</h1>' % resp
+
+
+@app.route('/now')
+def now_f(chat_id=None):
+    resp = datetime.datetime.now().strftime('%a %b %d %T %Y')
+
+    if chat_id:
+        if random.choice([0, 1]) == 0:
+            return send_reply({'chat_id': chat_id, 'sticker': 'BQADAgADeAcAAlOx9wOjY2jpAAHq9DUC'})
+        return send_reply({'chat_id': chat_id, 'text': resp})
     return '<h1>%s</h1>' % resp
