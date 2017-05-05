fa772d8fdf2e1a23702f5802163123543fe5510c
kapnuu <pryanichnikov.av@gmail.com>
2017-05-06 01:10:10 +0300

    fix weather view

--- app/views.py
+++ app/views.py
@@ -144,7 +144,7 @@ def weather_f(chat_id=None):
         ico = 'https://%s.herokuapp.com/weather-ico/%s.png' % (config.Config.HEROKU_APP_NAME,
                                                                weather['weather'][0]['icon'])
         t = weather['main']['temp']
-        resp = '''<a href="%s"><b>%s</b>: %s</a>
+        resp = '''<b>%s</b>: <a href="%s">%s</a>
 %s%s°C %s
 %s UTC''' % (ico, weather['name'], weather['weather'][0]['main'], '-' if t < 0 else '', t,
              weather['weather'][0]['description'].capitalize(), dt(weather['dt']).strftime('%a %b %d %H:%M %Y'))
