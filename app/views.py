25a714ce878edc03121820954bdd93cbe37fe5e2
kapnuu <pryanichnikov.av@gmail.com>
2017-05-06 00:28:54 +0300

    debug

--- app/views.py
+++ app/views.py
@@ -264,6 +264,6 @@ def test_img_f(chat_id):
     ret = send_reply({'method': 'sendPhoto',
                       'chat_id': chat_id,
                       'caption': 'Test',
-                      'photo': 'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Bierbrauer.jpg/220px-Bierbrauer.jpg'})
+                      'photo': 'https://kapnuu-bot.herokuapp.com/weather-ico/03n.png'})
     # log.info(ret)
     return ret
