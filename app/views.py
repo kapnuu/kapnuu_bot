6114c70f00169829a8f4e907245b6875d03f3ea2
kapnuu <pryanichnikov.av@gmail.com>
2017-05-06 19:34:24 +0300

    fix BOT_TZ_OFFSET type

--- app/views.py
+++ app/views.py
@@ -10,7 +10,7 @@ from os import path
 log = logging.getLogger('app')
 
 #TODO use user timezone
-tz_offset = 3 * 60 * 60 - app.config.get('BOT_TZ_OFFSET')
+tz_offset = 3 * 60 * 60 - int(app.config.get('BOT_TZ_OFFSET'))
 
 
 def dt(u): return datetime.datetime.fromtimestamp(u)
