41b6fcd22a72cc10657edf49644fac806d37dc04
kapnuu <pryanichnikov.av@gmail.com>
2017-06-20 01:44:48 +0300

    debug

--- app/openweathermap.py
+++ app/openweathermap.py
@@ -149,6 +149,7 @@ def get_forecast():
     if response.status_code == 200:
         forecast = json.loads(response.text)
         for n in next1:
+            print(n)
             timestamp = calendar.timegm(n[0].utctimetuple()) - 3 * 60 * 60
             item = next((x for x in forecast['list'] if x['dt'] == timestamp), None)
             if item:
